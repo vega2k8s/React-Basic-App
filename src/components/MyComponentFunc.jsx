95d577ef4ee30866affc01b7bf7a68825a542eb5
vega2k8s <vega2k8s@gmail.com>
2025-05-21 14:06:38 +0900

    MyComponent handleChange

--- src/components/MyComponentFunc.jsx
+++ src/components/MyComponentFunc.jsx
@@ -3,6 +3,17 @@ import { useState } from "react";
 const MyComponentFunc = ({ name, children }) => {
     //상태변수와 setter 함수 선언하기
     const [value, setValue] = useState(0);
+    // const [message, setMessage] = useState('');
+    // const [username, setUsername] = useState('');
+    const [inputs, setInputs] = useState({
+        message: '', username: ''
+    });
+
+    const handleChange = (e) => {
+
+    };
+
+    const { message, username } = inputs;
 
     return (
         <div>
@@ -10,8 +21,13 @@ const MyComponentFunc = ({ name, children }) => {
             <h3>Hello! {name}</h3>
             {children}
             <p>상태변수 value = {value}</p>
-            <button onClick={()=>(setValue(value + 1))}>증가</button>
-            <button onClick={()=>(setValue(value - 1))}>감소</button>
+            <button onClick={() => (setValue(value + 1))}>증가</button>
+            <button onClick={() => (setValue(value - 1))}>감소</button>
+            <p>State message의 값 = {message}</p>
+            <input name="message" value={message} onChange={handleChange} /><br />
+            <p>State username의 값 = {username}</p>
+            <input name="username" value={username} onChange={handleChange} /><br />
+
         </div>
     );
 };
