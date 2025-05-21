6a2790489450e0f0bebb644ac196f1f66f11b624
vega2k8s <vega2k8s@gmail.com>
2025-05-21 11:12:59 +0900

    MyComponent state add

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -2,13 +2,23 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class MyComponent extends Component {
-  render() {
+   //상태변수를 포함하는 state 객체 선언
+   state = {
+    value: 0,
+   }
+
+  render() {    
     //destructuring assignment
     const { name,age } = this.props;
+    const { value } = this.state;
+
     return (
       <div>
         <h2>클래스형 컴포넌트</h2>
         <h3>Hello! {name} / {age}</h3>
+        <p>상태변수 value = {value}</p>
+        <button>증가</button>
+        <button>감소</button>
       </div>
     )
   }
