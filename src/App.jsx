f29fe3da4409a7cb54bea2906eb70a95f23f3c50
vega2k8s <vega2k8s@gmail.com>
2025-05-21 09:25:24 +0900

    react hello app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  let text = "Dev-Server"
+  return (
+    <>
+      <h1>Hello ReactJS</h1>
+      <h2>Hello2 {text}</h2>
+    </>
+  )
+}
+
+export default App
