ba58ddeedb99344d7f35d99f90b4e6587f5b186f
vega2k8s <vega2k8s@gmail.com>
2025-05-21 13:22:13 +0900

    MyComponentFunc useState

--- src/components/MyComponentFunc.jsx
+++ src/components/MyComponentFunc.jsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 const MyComponentFunc = ({ name, children }) => {
     //상태변수와 setter 함수 선언하기
     const [value, setValue] = useState(0);
-        
+
     return (
         <div>
             <h2>함수형 컴포넌트</h2>
             <h3>Hello! {name}</h3>
             {children}
+            <p>상태변수 value = {value}</p>
+            <button onClick={()=>(setValue(value + 1))}>증가</button>
+            <button onClick={()=>(setValue(value - 1))}>감소</button>
         </div>
     );
 };
